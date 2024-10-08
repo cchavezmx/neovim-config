731255c8fb5c333488e4497e332895cda52e0e40
cchavezmx99 <carlos.chavez@99minutos.com>
2024-10-07 19:34:09 -0600

    mongoos actualizacion

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,8 +1,21 @@
 return {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
+  config = function()
+    vim.g.mkdp_auto_start = 1
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_refresh_slow = 1
+    vim.g.mkdp_command_for_global = 0
+    vim.g.mkdp_open_to_the_world = 0
+    vim.g.mkdp_browser = "firefox"
+    vim.g.mkdp_preview_options = {
+      mkit = {},
+      katex = {},
+      uml = {},
+      disable_sync_scroll = 0,
+      sync_scroll_type = "middle",
+      hide_yaml_meta = 1,
+      sequence_diagrams = {},
+      flowchart_diagrams = {},
+    }
   end,
 }
