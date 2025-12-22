4657abaf09aae51bef72370eccaac34a47f2576c
cchavezmx99 <carlos.chavez@99minutos.com>
2025-12-22 02:32:30 -0600

    nuevos cambios

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,43 +1,28 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"rcarriga/nvim-dap-ui",
 		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
 	},
 	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		local dapgo = require("dap-go")
+		require("dapui").setup()
+		require("dap-go").setup()
 
-		dapui.setup()
-		dapgo.setup()
+		local dap, dapui = require("dap"), require("dapui")
+
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
 		end
 		dap.listeners.before.launch.dapui_config = function()
 			dapui.open()
 		end
-
-		-- Include the next few lines until the comment only if you feel you need it
 		dap.listeners.before.event_terminated.dapui_config = function()
 			dapui.close()
 		end
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 		end
-		-- Include everything after this
-		vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>dr",
-			":lua require('dap').open({ restart = true })<CR>",
-			{ noremap = true, silent = true }
-		)
-		vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError", linehl = "", numhl = "" })
+
 		dap.configurations.go = {
 			{
 				type = "go",
@@ -47,7 +32,7 @@ return {
 				mode = "debug",
 				outputMode = "remote",
 			},
-      {
+			{
 				type = "go",
 				name = "Debug wms",
 				request = "launch",
@@ -80,5 +65,11 @@ return {
 				outputMode = "remote",
 			},
 		}
+
+		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
 	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,49 @@
 vim.g.mapleader = " "
 
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "nvim-telescope/telescope-fzf-native.nvim",
-    },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
 
-      -- Configuración base
-      telescope.setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
+			-- Configuración base
+			telescope.setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-      -- Keymaps
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>rg", ":Telescope registers<CR>", {})
-      vim.keymap.set("n", "<leader>fp", ":Telescope git_files<CR>", {})
+			-- Keymaps
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>rg", ":Telescope registers<CR>", {})
+			vim.keymap.set("n", "<leader>fp", ":Telescope git_files<CR>", {})
 
-      telescope.load_extension("ui-select")
-      vim.keymap.set("n", "<leader>pi", ":TelescopeImagePreview<CR>", { desc = "Preview imágenes con Kitty" })
-    end,
-  },
+			telescope.load_extension("ui-select")
+			vim.keymap.set("n", "<leader>pi", ":TelescopeImagePreview<CR>", { desc = "Preview imágenes con Kitty" })
+
+			vim.keymap.set("n", "<leader>ff", function()
+				builtin.find_files({
+					hidden = true,
+					no_ignore = true,
+					no_ignore_parent = true,
+				})
+			end, { desc = "Find files (incluye ocultos)" })
+		end,
+	},
 }
